7477584f11e581698d1bbecd7ff135e1f1fe4d03
camilo <cortiz20@amherst.edu>
2020-01-27 20:04:10 -0500

    Updates

---
+++ precache-manifest.4a9dfe6a6a44f85c88a1e44b8a33219f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f8400bf2b2e4272ae17acc2dc197be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0994f71a98548fdd7382",
+    "url": "./static/css/2.a0aa317c.chunk.css"
+  },
+  {
+    "revision": "1eac6f6536c3670815d1",
+    "url": "./static/css/main.3bf0de86.chunk.css"
+  },
+  {
+    "revision": "0994f71a98548fdd7382",
+    "url": "./static/js/2.c2b11ad3.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "./static/js/2.c2b11ad3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1eac6f6536c3670815d1",
+    "url": "./static/js/main.354d64f7.chunk.js"
+  },
+  {
+    "revision": "534b7005fa501f4692da",
+    "url": "./static/js/runtime-main.903d2eab.js"
+  },
+  {
+    "revision": "6408dacb1b8b8640dbdfcac00b663deb",
+    "url": "./static/media/logo.6408dacb.svg"
+  }
+]);
