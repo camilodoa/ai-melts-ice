584b67ee8bf1b3e4493445906483f6f9e2e2e181
camilodoa <camilodortiz@gmail.com>
2020-02-23 16:35:54 -0500

    Add CNAME, remove name

--- front/src/components/footer.js
+++ front/src/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className='footer m-3'>
       <div>
         <p>
-          Site and model made and trained with <span role='img' aria-label='Heart'>❤️</span> by <a href='https://camilodoa.ml'>@camilodoa</a>
+          Site and model made and trained with <span role='img' aria-label='Heart'>❤️</span>
         </p>
         <p>
           Icon made by <a href='https://www.flaticon.com/authors/freepik' title='Freepik'>Freepik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a>
